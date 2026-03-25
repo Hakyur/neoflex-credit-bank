6d23409b13693595a503c4ea6082ed19bf8e0653
Dmitry Rogotovsky <drogotovsky@gmail.com>
2026-03-26 00:23:34 +0300

    MVP-2: add JPA repositories for client and statement

---
+++ credit-bank/deal/src/main/java/ru/rogotovsky/deal/repository/StatementRepository.java
@@ -0,0 +1,9 @@
+package ru.rogotovsky.deal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.rogotovsky.deal.entity.Statement;
+
+@Repository
+public interface StatementRepository extends JpaRepository<Statement, Integer> {
+}

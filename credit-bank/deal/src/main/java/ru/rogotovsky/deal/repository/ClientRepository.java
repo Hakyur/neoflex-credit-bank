47e9d70e3ff2a0a2c0edadef46be4ae7408ca0a0
Dmitry Rogotovsky <drogotovsky@gmail.com>
2026-03-26 01:28:01 +0300

    MVP-2: fix id type in repositories

--- credit-bank/deal/src/main/java/ru/rogotovsky/deal/repository/ClientRepository.java
+++ credit-bank/deal/src/main/java/ru/rogotovsky/deal/repository/ClientRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.rogotovsky.deal.entity.Client;
 
+import java.util.UUID;
+
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Integer> {
+public interface ClientRepository extends JpaRepository<Client, UUID> {
 }
--- credit-bank/deal/src/main/java/ru/rogotovsky/deal/repository/StatementRepository.java
+++ credit-bank/deal/src/main/java/ru/rogotovsky/deal/repository/StatementRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.rogotovsky.deal.entity.Statement;
 
+import java.util.UUID;
+
 @Repository
-public interface StatementRepository extends JpaRepository<Statement, Integer> {
+public interface StatementRepository extends JpaRepository<Statement, UUID> {
 }

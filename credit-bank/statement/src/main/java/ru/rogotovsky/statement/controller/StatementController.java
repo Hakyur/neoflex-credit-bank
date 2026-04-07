9d2462006e5d55558b02b28dbcfc72a9b4d1587a
Dmitry Rogotovsky <drogotovsky@gmail.com>
2026-04-07 19:57:52 +0300

    implement /statement endpoint

--- credit-bank/statement/src/main/java/ru/rogotovsky/statement/controller/StatementController.java
+++ credit-bank/statement/src/main/java/ru/rogotovsky/statement/controller/StatementController.java
@@ -1,6 +1,7 @@
 package ru.rogotovsky.statement.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,16 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.rogotovsky.statement.dto.LoanOfferDto;
 import ru.rogotovsky.statement.dto.LoanStatementRequestDto;
+import ru.rogotovsky.statement.service.StatementService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/statement")
+@RequiredArgsConstructor
 public class StatementController {
+
+    private final StatementService statementService;
 
     @PostMapping
     public ResponseEntity<List<LoanOfferDto>> getLoanOffers(@Valid @RequestBody LoanStatementRequestDto requestDto) {
-        return null;
+        return ResponseEntity.ok(statementService.getLoanOffers(requestDto));
     }
 
     @PostMapping("/offer")
---
+++ credit-bank/statement/src/main/java/ru/rogotovsky/statement/service/StatementService.java
@@ -0,0 +1,20 @@
+package ru.rogotovsky.statement.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.rogotovsky.statement.client.DealClient;
+import ru.rogotovsky.statement.dto.LoanOfferDto;
+import ru.rogotovsky.statement.dto.LoanStatementRequestDto;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StatementService {
+
+    private final DealClient dealClient;
+
+    public List<LoanOfferDto> getLoanOffers(LoanStatementRequestDto requestDto) {
+        return dealClient.getLoanOffers(requestDto);
+    }
+}
